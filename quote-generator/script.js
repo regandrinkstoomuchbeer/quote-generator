702f34ed8bdfc2d773f9e95aa332719a4139f7fb
Regan Jansen <61661064+regandrinkstoomuchbeer@users.noreply.github.com>
2022-09-03 13:15:57 +0200

    Add files via upload

---
+++ quote-generator/script.js
@@ -0,0 +1,63 @@
+// API //
+
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newquoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+let apiQuotes = [];
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+function newQuote() {
+    loading();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else 
+    authorText.textContent = quote.author
+
+    if (!quote.text.length > 50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent =quote.text
+    complete();
+}
+
+async function getQuotes() {
+    loading();
+    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+    try {
+        const response = await fetch (apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {       
+    }
+}
+
+// Tweet quote //
+
+function tweetQuote() {
+
+    const twitterUrl = `https://twitter.com/intent/tweet?text?${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+} 
+
+// Event Listeners //
+
+newquoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+ getQuotes()
